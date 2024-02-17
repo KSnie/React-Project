9f2a58503511662c5e9dd63562e40d51d6793aee
ncwjsp <nc.wjsp@gmail.com>
2024-02-18 00:42:18 +0700

    Setup initial objects for my projects page

--- src/component/pages/Myproject.jsx
+++ src/component/pages/Myproject.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import "../css-pages/Myproject.css";
+import data from "./projects.json";
 
 const Myproject = () => {
   return (
-    <div>
-      <h2>Myproject Page</h2>
-      {/* Add content for the Myproject page */}
-      <p>This is the content of the Myproject Page.</p>
+    <div className="projects-content">
+      <div className="projects-header">
+        <h2>My Projects</h2>
+        <button>New Project</button>
+      </div>
+      <div className="projects-container">
+        {data.map((data) => (
+          <Project
+            name={data.name}
+            category={data.category}
+            img={data.img}
+            key={data.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Project = ({ name, category, img }) => {
+  console.log(img);
+  return (
+    <div className="project">
+      <img src={img} alt={name} />
+      <div className="project-description">
+        <p>
+          <strong>{name}</strong>
+        </p>
+        <span>{category}</span>
+      </div>
     </div>
   );
 };
