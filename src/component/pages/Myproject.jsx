9ab263b1bc965a7539bd4c07a066c461360845d1
KSnie <u6510348@au.edu>
2024-02-18 15:55:20 +0700

    Fix Myproject-Edit

--- src/component/pages/Myproject.jsx
+++ src/component/pages/Myproject.jsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
 import "../css-pages/Myproject.css";
-import data from "./projects.json";
 import { createPortal } from "react-dom";
 
 const Myproject = () => {
+  const MyprojectData = [
+    {
+      name: "Conjuring",
+      category: "Horror",
+      img: "/projects/conjuring.jpg",
+    },
+    {
+      name: "Joker",
+      category: "Drama",
+      img: "/projects/joker.jpg",
+    },
+    {
+      name: "Avengers",
+      category: "Action",
+      img: "/projects/avengers.jpg",
+    },
+    {
+      name: "Peaky Blinders",
+      category: "Historical",
+      img: "/projects/peakyblinders.jpg",
+    },
+    {
+      name: "Tarzan",
+      category: "Adventure",
+      img: "/projects/tarzan.jpg",
+    },
+  ];
+
   return (
     <div className="projects-content">
       <div className="projects-header">
@@ -11,7 +38,7 @@ const Myproject = () => {
         <button>New Project</button>
       </div>
       <div className="projects-container">
-        {data.map((data) => (
+        {MyprojectData.map((data) => (
           <Project data={data} key={data.name} />
         ))}
       </div>
@@ -22,9 +49,14 @@ const Myproject = () => {
 const Project = ({ data }) => {
   const [editProjectWindow, setEditProjectWindow] = useState(false);
   const [name, setName] = useState(data.name);
+  const [category, setCategory] = useState(data.category);
 
   function handleName(newName) {
     setName(newName);
+  }
+
+  function handleCategory(newCategory) {
+    setCategory(newCategory);
   }
 
   return (
@@ -40,32 +72,34 @@ const Project = ({ data }) => {
           <p>
             <strong>{name}</strong>
           </p>
-          <span>{data.category}</span>
+          <span>{category}</span>
         </div>
       </div>
       <EditProject
-        name={name}
-        category={data.category}
-        img={data.img}
+        data={data}
         isOpened={editProjectWindow}
         onClose={() => setEditProjectWindow(false)}
         handleName={handleName}
+        handleCategory={handleCategory}
       />
     </>
   );
 };
 
 const EditProject = ({
-  name,
-  category,
-  img,
+  data,
   isOpened,
   onClose,
   handleName,
+  handleCategory,
 }) => {
+  const [editedName, setEditedName] = useState(data.name);
+  const [editedCategory, setEditedCategory] = useState(data.category);
+
   if (!isOpened) {
     return null;
   }
+
   return createPortal(
     <div>
       <div className="overlay">
@@ -75,20 +109,26 @@ const EditProject = ({
               Close
             </span>
             <div className="modal-content">
-              <img src={img} alt={name} />
+              <img src={data.img} alt={data.name} />
               <form
                 onSubmit={(e) => {
                   e.preventDefault();
-                  handleName(e.target.value);
+                  handleName(editedName);
+                  handleCategory(editedCategory);
                 }}
               >
                 <input
                   type="text"
-                  placeholder="Name"
-                  value={name}
-                  onChange={(e) => e.target.value}
+                  placeholder={data.name}
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
                 />
-                <input type="text" placeholder="Category" value={category} />
+                <input
+                  type="text"
+                  placeholder={data.category}
+                  value={editedCategory}
+                  onChange={(e) => setEditedCategory(e.target.value)}
+                />
                 <button>Update</button>
               </form>
             </div>
@@ -99,5 +139,6 @@ const EditProject = ({
     document.getElementById("modal")
   );
 };
+
 
 export default Myproject;
