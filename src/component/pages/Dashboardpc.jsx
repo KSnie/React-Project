3a64de75248e8f551d5e21269e435f31e3f96d4a
KSnie <u6510348@au.edu>
2024-02-19 19:10:31 +0700

    Complete Dashboard Project

--- src/component/pages/Dashboardpc.jsx
+++ src/component/pages/Dashboardpc.jsx
@@ -1,11 +1,101 @@
 import React from "react";
+import profile from "../image/profile.svg";
+import profile2 from "../image/profile2.svg";
+import "../css-pages/Dashboard.css";
 
 const Dashboardpc = () => {
+
+
+  const DashboardDetail = [
+    {
+      userProfile_id: profile,
+      nameStudio: "Marvel Studios1",
+      category: "Horror",
+      viewer: "500",
+      Register: "100"
+    },
+    {
+      userProfile_id: profile,
+      nameStudio: "Marvel Studios2",
+      category: "Horror",
+      viewer: "500",
+      Register: "100"
+    },
+    {
+      userProfile_id: profile2,
+      nameStudio: "Marvel Studios3",
+      category: "Horror",
+      viewer: "500",
+      Register: "100"
+    },
+    {
+      userProfile_id: profile2,
+      nameStudio: "Marvel Studios3",
+      category: "Horror",
+      viewer: "500",
+      Register: "100"
+    },
+    {
+      userProfile_id: profile2,
+      nameStudio: "Marvel Studios3",
+      category: "Horror",
+      viewer: "500",
+      Register: "100"
+    },
+  ];
+
   return (
     <div>
-      <h2>Dashboardpc Page</h2>
-      {/* Add content for the Dashboardpc page */}
-      <p>This is the content of the Dashboardpc Page.</p>
+      <div className="Dashboard-container">
+        <div className="Script-header">
+          <h4>Dashboard Project</h4>
+          <p>Overview Page</p>
+        </div>
+
+        <div className="Dashboard-summary-content">
+          <div className="dashboard-user">
+            <h4>Register</h4>
+            <h2>1024</h2>
+          </div>
+          <div className="dashboard-Viewer">
+            <h4>Viewer</h4>
+            <h2>9400</h2>
+          </div>
+          <div className="dashboard-Register">
+            <h4>Today Register</h4>
+            <h2>180</h2>
+          </div>
+        </div>
+
+        <div className="Dashboard-main-content">
+          <div className="containter">
+            <h5 className="Dashboard-text">Today's trends</h5>
+            {DashboardDetail.map((dashboard, index) => (
+              <div key = {index} className="Dashboard-main-container">
+                <img src={dashboard.userProfile_id} alt="Profile"></img>
+
+                <div className="Dashboard-content-name">
+                  <h5>{dashboard.nameStudio}</h5>
+                  <p>Category {dashboard.category}</p>
+                </div>
+
+                <div className="Dashboard-content-Viewer">
+                  <h5>Viewer</h5>
+                  <p>{dashboard.viewer}</p>
+                </div>
+
+                <div className="Dashboard-content-Register">
+                  <h5>Register</h5>
+                  <p>{dashboard.Register}</p>
+                </div>
+
+                <button className="Dashboard-button-View">View</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
